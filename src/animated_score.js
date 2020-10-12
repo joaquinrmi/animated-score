a10721aa469cb5ec632c8c1abe39ba0df4b9c3b3
joaquinrmi <joaquinruaimi@gmail.com>
2020-10-11 22:14:44 -0300

    added score lines drawing

--- src/animated_score.js
+++ src/animated_score.js
@@ -9,6 +9,8 @@ class AnimatedScore
 	/*
 		"args" debe ser un objeto con los siguientes campos:
 			> containerId: (string) id del elemento que contendrá el <canvas> de la partitura.
+			> framerate: (number) (opcional) cantidad de cuadros por segundo. Por defecto es 60.
+			> playingVelocity: (number) (opcional) velocidad en pixeles/segundo de la animación, por defecto se establece en 200 pixeles/segundo. No afecta a la duración de las notas.
 	*/
 	constructor(args)
 	{
@@ -22,6 +24,24 @@ class AnimatedScore
 			throw "args.containerId debe ser un string";
 		}
 
+		if(typeof args.playingVelocity == "number")
+		{
+			this.velocity = args.playingVelocity;
+		}
+		else
+		{
+			this.velocity = 200;
+		}
+
+		if(typeof args.framerate == "number" && args.framerate > 0)
+		{
+			this.framerate = args.framerate;
+		}
+		else
+		{
+			this.framerate = 60;
+		}
+
 		this.scoreContainer = document.getElementById(args.containerId);
 		if(this.scoreContainer == null)
 		{
@@ -31,6 +51,10 @@ class AnimatedScore
 		this.canvasPadding = 20;
 		this.scoreHeight = 29;
 
+		/*
+			Creación del canvas.
+			Su ancho será igual al ancho del contenedor.
+		*/
 		this.canvas = document.createElement("canvas");
 
 		const containerDimensions = this.scoreContainer.getBoundingClientRect();
@@ -41,5 +65,60 @@ class AnimatedScore
 		this.scoreContainer.appendChild(this.canvas);
 
 		this.context = this.canvas.getContext("2d");
+
+		this.loopID = 0;
+		this.status = "stopped";
+	}
+
+	start()
+	{
+		if(this.status != "playing")
+		{
+			this.status = "playing";
+			this.loopID = setInterval(this.mainLoop.bind(this), 1000 / this.framerate);
+		}
+	}
+
+	pause()
+	{
+		this.status = "paused";
+		clearInterval(this.loopID);
+	}
+
+	stop()
+	{
+		this.status = "stopped";
+		this.reset();
+		clearInterval(this.loopID);
+	}
+
+	reset()
+	{}
+
+	mainLoop()
+	{
+		this.update();
+		this.draw();
+	}
+
+	update()
+	{}
+
+	draw()
+	{
+		this.drawScoreLines(this.context);
+	}
+
+	drawScoreLines(context)
+	{
+		for(var i = 0; i < 5; ++i)
+		{
+			context.beginPath();
+			context.moveTo(0, this.canvasPadding + i * 7 + 0.5);
+			context.lineTo(this.canvas.width, this.canvasPadding + i * 7 + 0.5);
+			context.strokeStyle = "black";
+			context.lineWidth = 1;
+			context.stroke();
+		}
 	}
 };
